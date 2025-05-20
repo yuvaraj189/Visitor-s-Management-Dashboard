2743daf18ca99c338d0237ff5f7dbebe5f0d1c10
yuvaraj189 <yuvarajkumar093@gmail.com>
2025-05-20 08:36:01 +0530

    error rectified-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import VisitorSignup from "./pages/VisitorSignup.jsx";
-import AdminLogin from "./pages/AdminLogin.jsx";
-import AdminDashboard from "./pages/AdminDashboard.jsx";
-import Navbar from "./components/Navbar.jsx";
+import VisitorSignup from "./pages/VisitorSignup";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import Navbar from "./components/Navbar";
 import './App.css'
 
 function App() {
